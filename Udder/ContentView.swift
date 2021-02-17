0ef72c37ac7277fc400cc643c66b7db122243d64
Ai Shibata <ai.shibata1@gmail.com>
2021-02-17 13:17:59 -0800

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Ai Shibata on 2/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
